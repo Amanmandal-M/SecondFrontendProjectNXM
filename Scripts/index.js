010ab58e576827fc64db3cac3aab3311197f2c19
Aman Kumar <amanmandal644@gmail.com>
2023-01-16 03:53:00 +0530

    Bug Fixed

--- Scripts/index.js
+++ Scripts/index.js
@@ -4,13 +4,13 @@ const LoginButton = document.querySelector(".loginButton")
 SignUpButton.addEventListener("click", ()=>{
     alert("Click Here To Go in Sign Up Page !");
     setTimeout(()=>{
-        location.pathname="/Frontend/Views/signUp.html"
+        location.pathname="/Views/signUp.html"
     },0)
 })
 
 LoginButton.addEventListener("click", ()=>{
     alert("Click Here To Go in Login Page !");
     setTimeout(()=>{
-        location.pathname="/Frontend/Views/login.html"
+        location.pathname="/Views/login.html"
     },0) 
 })
